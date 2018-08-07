02f36479068ffcc4bb818e6bacbb53821af489ab
yuanzhicheng <1439816794@qq.com>
2018-08-07 11:14:34 +0800

    配置路由router--第五次提交

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+// 导入模块  模块块语法 CommonJS
+// const Vue = require('vue');
+
+// ES6中模块化的语法
+import Vue from 'vue';
+import App from './App.vue';
 
 // 导入全局样式
 // 导入bootstrap
@@ -7,7 +11,11 @@ import 'bootstrap/dist/css/bootstrap.css';
 // 导入index.css
 import './assets/css/index.css';
 
+// 导入路由模块
+import router from './routes/router';
+
 new Vue({
+  router,
   el: '#app',
   render: h => h(App)
-})
+});
